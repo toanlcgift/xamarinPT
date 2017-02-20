4a80d71df30744d26ad749696c534941f51ddd3e
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2017-02-21 06:17:13 +0700

    update

---
+++ XamarinPT/XamarinPT/XamarinPT/App.cs
@@ -0,0 +1,51 @@
+﻿using PCLStorage;
+using System.IO;
+using System.Reflection;
+
+using Xamarin.Forms;
+
+namespace XamarinPT
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var content = new ContentPage
+            {
+                Title = "XamarinPT",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override async void OnStart()
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream(XamarinPT.Resources.EMBEDDED_KANJI_DB);
+            var file = await FileSystem.Current.LocalStorage.CreateFileAsync(XamarinPT.Resources.DB_NAME, CreationCollisionOption.ReplaceExisting);
+            Stream str = await file.OpenAsync(FileAccess.ReadAndWrite);
+            await stream.CopyToAsync(str);
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ XamarinPT/XamarinPT/XamarinPT/SqliteManager.cs
@@ -0,0 +1,59 @@
+﻿using PCLStorage;
+using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XamarinPT
+{
+    class SqliteManager
+    {
+        private static SQLiteConnection sqlc = new SQLiteConnection(FileSystem.Current.LocalStorage.Path + "\\" + Resources.DB_NAME);
+
+        private static SqliteManager _instance;
+        public static SqliteManager Instance()
+        {
+            _instance = _instance ?? (_instance = new SqliteManager());
+            return _instance;
+        }
+
+        public static int CreateTable<T>()
+        {
+            return sqlc.CreateTable<T>();
+        }
+
+        public static List<T> GetAllTable<T>() where T : new()
+        {
+            return sqlc.Table<T>().ToList();
+        }
+
+        public static int Insert(object input)
+        {
+            return sqlc.Insert(input);
+        }
+
+        public static int InsertAll(List<object> input)
+        {
+            return sqlc.InsertAll(input);
+        }
+
+        public static int Update(object input)
+        {
+            return sqlc.Update(input);
+        }
+
+        public static int UpdateAll(List<object> input)
+        {
+            return sqlc.UpdateAll(input);
+        }
+
+        public static int Delete(object input)
+        {
+            return sqlc.Delete(input);
+        }
+
+        public static int DeleteAllTable<T>()
+        {
+            return sqlc.DeleteAll<T>();
+        }
+    }
+}
