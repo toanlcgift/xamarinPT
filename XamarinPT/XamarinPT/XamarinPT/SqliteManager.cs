c87ac89e972f90c1db77e6236d229ffc5ff74812
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2018-11-29 11:28:05 +0700

    update

--- XamarinPT/XamarinPT/XamarinPT/SqliteManager.cs
+++ XamarinPT/XamarinPT/XamarinPT/SqliteManager.cs
@@ -1,13 +1,14 @@
 ﻿using PCLStorage;
 using SQLite;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace XamarinPT
 {
     class SqliteManager
     {
-        private static SQLiteConnection sqlc = new SQLiteConnection(FileSystem.Current.LocalStorage.Path + "\\" + Resources.DB_NAME);
+        private static SQLiteConnection sqlc = new SQLiteConnection(Path.Combine(FileSystem.Current.LocalStorage.Path, Resources.DB_NAME));
 
         private static SqliteManager _instance;
         public static SqliteManager Instance()
@@ -16,42 +17,42 @@ namespace XamarinPT
             return _instance;
         }
 
-        public static int CreateTable<T>()
+        public int CreateTable<T>()
         {
             return sqlc.CreateTable<T>();
         }
 
-        public static List<T> GetAllTable<T>() where T : new()
+        public List<T> GetAllTable<T>() where T : new()
         {
             return sqlc.Table<T>().ToList();
         }
 
-        public static int Insert(object input)
+        public int Insert(object input)
         {
             return sqlc.Insert(input);
         }
 
-        public static int InsertAll(List<object> input)
+        public int InsertAll(List<object> input)
         {
             return sqlc.InsertAll(input);
         }
 
-        public static int Update(object input)
+        public int Update(object input)
         {
             return sqlc.Update(input);
         }
 
-        public static int UpdateAll(List<object> input)
+        public int UpdateAll(List<object> input)
         {
             return sqlc.UpdateAll(input);
         }
 
-        public static int Delete(object input)
+        public int Delete(object input)
         {
             return sqlc.Delete(input);
         }
 
-        public static int DeleteAllTable<T>()
+        public int DeleteAllTable<T>()
         {
             return sqlc.DeleteAll<T>();
         }
